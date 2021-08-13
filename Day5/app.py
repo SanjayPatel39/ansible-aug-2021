37b8d57ad013ab50d4226f686ae31c48a80aaef8
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-08-13 09:07:18 +0000

    Added Day5 lab exercises.

--- Day5/app.py
+++ Day5/app.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET', 'POST'])
 def sayHello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='0.0.0.0', port=80)
