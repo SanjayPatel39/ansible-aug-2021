679f59f6de079653bc7b350bca4aa48fae15ef3f
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-08-13 06:19:14 +0000

    Added Day5 lab setup instructions.

---
+++ Day5/Hello/apptest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import unittest
+from app import sayHello
+
+class Testing(unittest.TestCase):
+    def test_sayHello(self):
+        actualResponse   = sayHello() 
+        expectedResponse = 'Hello World!'
+        self.assertEqual(actualResponse, expectedResponse)
+    
+if __name__ == '__main__':
+    unittest.main() 
---
+++ Day5/README.md
@@ -0,0 +1,15 @@
+### Download Jenkins Build Server
+```
+sudo su -
+wget https://get.jenkins.io/war-stable/2.289.3/jenkins.war
+```
+
+### Install JDK 1.8
+```
+yum install -y java-11-openjdk-devel
+```
+
+### Verify if JRE is in path
+```
+java -version
+```
